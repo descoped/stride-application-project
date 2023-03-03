cabf0911d15dff65b20acf4f3171ceffdc12662e
oranheim <oranheim@gmail.com>
2023-03-03 11:38:38 +0100

    Property map to Json converter

---
+++ stride-application/src/main/java/io/descoped/stride/application/support/PropertyMapToJsonConverter.java
@@ -0,0 +1,212 @@
+package io.descoped.stride.application.support;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Optional.ofNullable;
+
+public record PropertyMapToJsonConverter(Map<String, String> properties, ObjectNode json) {
+
+    public PropertyMapToJsonConverter(Map<String, String> propertyMap) {
+        this(propertyMap, JsonNodeFactory.instance.objectNode());
+
+        List<Property> propertyList = new ArrayList<>(propertyMap.size());
+        propertyMap.forEach((key, value) -> propertyList.add(new PropertyTokenizer(key, value).property));
+
+        Map<String, JsonNode> parentPathMap = new LinkedHashMap<>();
+        parentPathMap.put("ROOT", json);
+
+        Set<String> visitedPaths = new LinkedHashSet<>();
+
+        for (int i = 0; i < propertyList.size(); i++) {
+            Property property = propertyList.get(i);
+
+            for (int j = 0; j < property.elements.size(); j++) {
+                PropertyElement propertyElement = property.elements.get(j);
+
+                //String pathElements = property.elements.stream().map(m -> m.key() + "[" + m.type() + "]").collect(Collectors.joining(".")) + "=" + property.value;
+                String nextParentPathElements = getPathElementsByProperty(property, j + 1);
+                String parentPathElements = getPathElementsByProperty(property, j);
+
+                JsonNode parentNode = parentPathMap.get(parentPathElements);
+
+                JsonNode jsonNode = switch (propertyElement.type()) {
+                    case LEAF_NODE, ARRAY_ELEMENT -> JsonNodeFactory.instance.textNode(property.value);
+                    case OBJECT, ARRAY_OBJECT -> JsonNodeFactory.instance.objectNode();
+                    case ARRAY_NODE -> JsonNodeFactory.instance.arrayNode();
+                };
+
+                // skip already handled path
+                if (!visitedPaths.add(nextParentPathElements)) {
+                    continue;
+                }
+
+                // set next parentNode
+                JsonNode childNode = parentPathMap.computeIfAbsent(nextParentPathElements, k -> jsonNode);
+
+                if (parentNode instanceof ObjectNode parent) {
+                    parent.set(propertyElement.key(), childNode);
+
+                } else if (parentNode instanceof ArrayNode parent) {
+                    parent.add(childNode);
+
+                } else {
+                    throw new IllegalStateException("property-element-pos: " + i + " => " + parentNode);
+                }
+            }
+        }
+    }
+
+    private String getPathElementsByProperty(Property property, int limit) {
+        List<String> parentPathElementList = property.elements.stream().limit(limit).map(PropertyElement::key).toList();
+        return "ROOT" + (parentPathElementList.size() == 0 ? "" : "." + String.join(".", parentPathElementList));
+    }
+
+    record PropertyTokenizer(String key, String value, Property property) {
+        private final static Pattern INTEGER_PATTERN = Pattern.compile("^\\d+$");
+
+        PropertyTokenizer(String property, String value) {
+            this(property, value, tokenize(property, value));
+        }
+
+        static Property tokenize(String property, String value) {
+            List<PropertyElement> elementList = new ArrayList<>();
+
+            List<String> list = List.of(property.split("\\."));
+            String previous = null;
+            for (int i = 0; i < list.size(); i++) {
+                //String prev = (i - 1 >= 0) ? list.get(i - 1) : null;
+                String current = list.get(i);
+                String next = (i + 1 < list.size()) ? list.get(i + 1) : null;
+                //String nextNext = (i + 2 < list.size()) ? list.get(i + 2) : null;
+
+                if (isLeafNode(current, next)) {
+                    elementList.add(PropertyElement.of(current, ElementType.LEAF_NODE));
+
+                } else if (isArrayNode(previous, current) && isArrayObject(current, next)) {
+                    elementList.add(PropertyElement.of(current, ElementType.ARRAY_OBJECT));
+
+                } else if (isArrayNode(previous, current) && isArrayElement(current)) {
+                    elementList.add(PropertyElement.of(current, ElementType.ARRAY_ELEMENT));
+
+                } else if (isArrayNode(current, next)) {
+                    elementList.add(PropertyElement.of(current, ElementType.ARRAY_NODE));
+
+                } else if (isObject(current)) {
+                    elementList.add(PropertyElement.of(current, ElementType.OBJECT));
+                } else {
+                    throw new IllegalStateException(String.format("Unknown type: [elementIndex: %s] %s <- %s", i, current, i > 0 ? list.get(i - 1) : "(null)"));
+                }
+
+                previous = current;
+            }
+
+            return new Property(property, value, elementList);
+        }
+
+        private static boolean isLeafNode(String token, String nextToken) {
+            return isObject(token) && nextToken == null;
+        }
+
+        private static boolean isObject(String token) {
+            return !isArrayElement(token);
+        }
+
+        private static boolean isArrayNode(String token, String nextToken) {
+            return isObject(token) && isArrayElement(nextToken);
+        }
+
+        private static boolean isArrayElement(String token) {
+            return ofNullable(token)
+                    .map(INTEGER_PATTERN::matcher)
+                    .map(Matcher::find)
+                    .orElse(false);
+        }
+
+        private static boolean isArrayObject(String token, String nextToken) {
+            return isArrayElement(token) && (nextToken != null && isObject(nextToken));
+        }
+    }
+
+    enum ElementType {
+        LEAF_NODE,
+        OBJECT,
+        ARRAY_NODE,
+        ARRAY_ELEMENT,
+        ARRAY_OBJECT;
+    }
+
+
+    record Property(String key, String value, List<PropertyElement> elements) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Property property = (Property) o;
+            return key.equals(property.key) && Objects.equals(value, property.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+
+    interface PropertyElement {
+
+        String key();
+
+        ElementType type();
+
+        default boolean isLeafNode() {
+            return ElementType.LEAF_NODE.equals(type());
+        }
+
+        default boolean isObject() {
+            return ElementType.OBJECT.equals(type());
+        }
+
+        default boolean isArrayNode() {
+            return ElementType.ARRAY_NODE.equals(type());
+        }
+
+        default boolean isArrayElement() {
+            return ElementType.ARRAY_ELEMENT.equals(type());
+        }
+
+        default boolean isArrayObject() {
+            return ElementType.ARRAY_OBJECT.equals(type());
+        }
+
+        record PropertyElementImpl(String key, ElementType type) implements PropertyElement {
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                PropertyElementImpl that = (PropertyElementImpl) o;
+                return Objects.equals(key, that.key) && type == that.type;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(key, type);
+            }
+        }
+
+        static PropertyElement of(String property, ElementType type) {
+            return new PropertyElementImpl(property, type);
+        }
+    }
+}
---
+++ stride-application/src/test/java/io/descoped/stride/application/support/JsonConverterTest.java
@@ -0,0 +1,204 @@
+package io.descoped.stride.application.support;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import no.cantara.config.ApplicationProperties;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+
+import static io.descoped.stride.application.support.PropertyMapToJsonConverter.*;
+import static java.util.Optional.ofNullable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JsonConverterTest {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonConverterTest.class);
+
+    @Test
+    void testJacksonDataformatProperties() throws IOException {
+        ApplicationProperties properties = ApplicationProperties.builder()
+                .classpathPropertiesFile("application-test.properties")
+                .testDefaults()
+                .build();
+
+        Map<String, String> metadata = properties.subMap("metadata");
+
+        JavaPropsMapper mapper = new JavaPropsMapper();
+        JsonNode root = mapper.readMapAs(metadata, JsonNode.class);
+        log.info("{}", root.toPrettyString());
+
+        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+        log.info("{}", yamlMapper.writeValueAsString(root));
+    }
+
+    @Test
+    void loadPropertiesAndConvertToJson() {
+        ApplicationProperties config = ApplicationProperties.builder()
+                .classpathPropertiesFile("application-test.properties")
+                .build();
+
+        PropertyMapToJsonConverter converter = new PropertyMapToJsonConverter(config.subMap("metadata"));
+        ObjectNode json = converter.json();
+        //log.info("\n{}", json.toPrettyString());
+        assertEquals("v1", json.get("a").asText());
+        assertEquals("v1", json.get("b").asText());
+        ArrayNode cArrayNode = (ArrayNode) json.get("c");
+        assertEquals("v1", cArrayNode.get(0).asText());
+        assertEquals("v2", cArrayNode.get(1).asText());
+        assertEquals("v1", cArrayNode.get(2).get("prop1").asText());
+        assertEquals("v1", cArrayNode.get(2).get("prop2").asText());
+        JsonNode prop3ArrayNodeNodeWithArrayElements = cArrayNode.get(3).get("prop3");
+        assertEquals("v1", prop3ArrayNodeNodeWithArrayElements.get(0).asText());
+        assertEquals("v2", prop3ArrayNodeNodeWithArrayElements.get(1).asText());
+        ArrayNode dArrayNode = (ArrayNode) json.get("d");
+        ArrayNode prop1ArrayNodeNodeWithArrayObjects = (ArrayNode) dArrayNode.get(0).get("prop1");
+        assertEquals("v1", prop1ArrayNodeNodeWithArrayObjects.get(0).get("foo").asText());
+        assertEquals("v2", prop1ArrayNodeNodeWithArrayObjects.get(0).get("bar").asText());
+        ArrayNode dArrayNodeWithArrayObjectProp2 = (ArrayNode) dArrayNode.get(0).get("prop2");
+        assertEquals("v1", dArrayNodeWithArrayObjectProp2.get(0).get("bar").asText());
+    }
+
+
+    @Test
+    void testTokenizer() {
+        {
+            String[] propertyAndValue = {"a", "v"};
+
+            Property property = PropertyTokenizer.tokenize(propertyAndValue[0], propertyAndValue[1]);
+            assertEquals(1, property.elements().size());
+
+            Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+            Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+            assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.LEAF_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            assertEquals("v", property.value());
+        }
+
+        {
+            String[] propertyWithObjectAndValue = {"a.b", "v"};
+
+            Property property = PropertyTokenizer.tokenize(propertyWithObjectAndValue[0], propertyWithObjectAndValue[1]);
+            assertEquals(2, property.elements().size());
+
+            Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+            Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+            assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = propertyElement = ofNullable(elementIterator.next());
+            assertEquals("b", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.LEAF_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            assertEquals("v", property.value());
+        }
+
+        {
+            String[] propertyWithObjectAndArrayValues = {"a.c.0", "v1", "a.c.1", "v2"};
+
+            {
+                Property property = PropertyTokenizer.tokenize(propertyWithObjectAndArrayValues[0], propertyWithObjectAndArrayValues[1]);
+                assertEquals(3, property.elements().size());
+
+                Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+                Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+                assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+                propertyElement = ofNullable(elementIterator.next());
+                assertEquals("c", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.ARRAY_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+                propertyElement = ofNullable(elementIterator.next());
+                assertEquals("0", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.ARRAY_ELEMENT, propertyElement.map(PropertyElement::type).orElse(null));
+
+                assertEquals("v1", property.value());
+            }
+
+            {
+                Property property = PropertyTokenizer.tokenize(propertyWithObjectAndArrayValues[2], propertyWithObjectAndArrayValues[3]);
+                assertEquals(3, property.elements().size());
+
+                Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+                Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+                assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+                propertyElement = ofNullable(elementIterator.next());
+                assertEquals("c", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.ARRAY_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+                propertyElement = ofNullable(elementIterator.next());
+                assertEquals("1", propertyElement.map(PropertyElement::key).orElse(null));
+                assertEquals(ElementType.ARRAY_ELEMENT, propertyElement.map(PropertyElement::type).orElse(null));
+
+                assertEquals("v2", property.value());
+            }
+        }
+
+        String[] propertyWithObjectAndArrayObjects = {"a.d.0.p1", "v1", "a.d.0.p2", "v2"};
+        {
+            Property property = PropertyTokenizer.tokenize(propertyWithObjectAndArrayObjects[0], propertyWithObjectAndArrayObjects[1]);
+            assertEquals(4, property.elements().size());
+
+            Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+            Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+            assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("d", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.ARRAY_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("0", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.ARRAY_OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("p1", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.LEAF_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            assertEquals("v1", property.value());
+        }
+        {
+            Property property = PropertyTokenizer.tokenize(propertyWithObjectAndArrayObjects[2], propertyWithObjectAndArrayObjects[3]);
+            assertEquals(4, property.elements().size());
+
+            Iterator<PropertyElement> elementIterator = property.elements().iterator();
+
+            Optional<PropertyElement> propertyElement = ofNullable(elementIterator.next());
+            assertEquals("a", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("d", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.ARRAY_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("0", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.ARRAY_OBJECT, propertyElement.map(PropertyElement::type).orElse(null));
+
+            propertyElement = ofNullable(elementIterator.next());
+            assertEquals("p2", propertyElement.map(PropertyElement::key).orElse(null));
+            assertEquals(ElementType.LEAF_NODE, propertyElement.map(PropertyElement::type).orElse(null));
+
+            assertEquals("v2", property.value());
+        }
+    }
+}
