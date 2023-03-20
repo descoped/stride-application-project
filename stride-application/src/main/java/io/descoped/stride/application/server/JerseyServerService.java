3cdd43b1076ba1138516dc641e8a0da4f09515aa
oranheim <oranheim@gmail.com>
2023-03-20 07:27:57 +0100

    Added Jersey server service

---
+++ stride-application/src/main/java/io/descoped/stride/application/server/JerseyServerService.java
@@ -0,0 +1,27 @@
+package io.descoped.stride.application.server;
+
+import io.descoped.stride.application.config.ApplicationConfiguration;
+import jakarta.inject.Inject;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.glassfish.hk2.api.PreDestroy;
+import org.glassfish.hk2.runlevel.RunLevel;
+import org.jvnet.hk2.annotations.Service;
+
+@Service(name = "jersey.server")
+@RunLevel(5)
+public class JerseyServerService implements PreDestroy {
+
+//    private final JerseyServletContainer servletContainer;
+
+    @Inject
+    public JerseyServerService(ApplicationConfiguration configuration,
+                               ServletContextHandler ctx) throws Exception {
+
+
+    }
+
+    @Override
+    public void preDestroy() {
+
+    }
+}
