71f8fd3588a5dd2d31ba294ae760d81ce2ac65ca
oranheim <oranheim@gmail.com>
2023-03-23 15:30:23 +0100

    Deployment config

--- stride-application/src/main/java/io/descoped/stride/application/config/Deployment.java
+++ stride-application/src/main/java/io/descoped/stride/application/config/Deployment.java
@@ -6,8 +6,20 @@ import io.descoped.stride.application.jackson.JsonElement;
 
 public record Deployment(ObjectNode json) {
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Services services() {
+        return new Services(JsonElement.of(json).with("services").array());
+    }
+
     public Servlets servlets() {
         return new Servlets(JsonElement.of(json).with("servlets").array());
+    }
+
+    public Filters filters() {
+        return new Filters(JsonElement.of(json).with("filters").array());
     }
 
     public record Builder(ObjectNode builder) {
@@ -16,8 +28,18 @@ public record Deployment(ObjectNode json) {
             this(JsonNodeFactory.instance.objectNode());
         }
 
+        public Builder services(Services.Builder servicesBuilder) {
+            builder.set("services", servicesBuilder.build().json());
+            return this;
+        }
+
         public Builder servlets(Servlets.Builder servletsBuilder) {
             builder.set("servlets", servletsBuilder.build().json());
+            return this;
+        }
+
+        public Builder filters(Filters.Builder filtersBuilder) {
+            builder.set("filters", filtersBuilder.build().json());
             return this;
         }
 
--- stride-application/src/test/java/io/descoped/stride/application/config/DeploymentTest.java
+++ stride-application/src/test/java/io/descoped/stride/application/config/DeploymentTest.java
@@ -1,16 +1,24 @@
 package io.descoped.stride.application.config;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.descoped.stride.application.server.JerseyServerService;
 import io.descoped.stride.application.server.JettyServerService;
 import io.dropwizard.metrics.servlets.AdminServlet;
 import io.dropwizard.metrics.servlets.MetricsServlet;
 import jakarta.servlet.DispatcherType;
+import no.cantara.config.ApplicationProperties;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hk2.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -102,5 +110,76 @@ class DeploymentTest {
         assertEquals("io.dropwizard.metrics.servlets.MetricsServlet", servlets.servletName("metrics")
                 .map(Servlets.Servlet::className)
                 .orElse(null));
+    }
+
+    @Test
+    void deploymentBuilder() {
+        Deployment deployment = Deployment.builder()
+                .services(Services.builder()
+                        .service(Services.serviceBuilder()
+                                .name("jetty.server")
+                                .clazz(JettyServerService.class))
+                        .service(Services.serviceBuilder()
+                                .clazz(JerseyServerService.class))
+                        .service(Services.serviceBuilder()
+                                .name("dummy.service")
+                                .clazz(Service.class)
+                                .metadata(Services.metadataBuilder()
+                                        .property("foo", "bar"))))
+                .filters(Filters.builder()
+                        .filter(Filters.filterBuilder()
+                                .name("dummy")
+                                .clazz(jakarta.servlet.Filter.class)
+                                .pathSpec("/dummy")
+                                .dispatches(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST))))
+                .servlets(Servlets.builder()
+                        .servlet(Servlets.servletBuilder()
+                                .clazz(AdminServlet.class)
+                                .pathSpec("/admin"))
+                        .servlet(Servlets.servletBuilder()
+                                .name("metrics")
+                                .clazz(MetricsServlet.class)
+                                .pathSpec("/metrics")))
+                .build();
+        log.debug("\n{}", deployment.json().toPrettyString());
+    }
+
+    @Test
+    void propertiesToDeployment() throws IOException {
+        final String props = """
+                services.0.serviceName=jetty.server
+                services.0.serviceClass=io.descoped.stride.application.server.JettyServerService
+                services.1.serviceClass=io.descoped.stride.application.server.JerseyServerService
+                services.2.serviceName=dummy.service
+                services.2.serviceClass=org.jvnet.hk2.annotations.Service
+                services.2.metadata.foo=bar
+                filters.0.name=dummy
+                filters.0.filterClass=jakarta.servlet.Filter
+                filters.0.pathSpec=/dummy
+                filters.0.dispatches.0=FORWARD
+                filters.0.dispatches.1=REQUEST
+                servlets.0.servletClass=io.dropwizard.metrics.servlets.AdminServlet
+                servlets.0.pathSpec=/admin
+                servlets.1.servletName=metrics
+                servlets.1.servletClass=io.dropwizard.metrics.servlets.MetricsServlet
+                servlets.1.pathSpec=/metrics
+                """;
+
+        Properties properties = new Properties();
+        properties.load(new StringReader(props));
+
+        Map<String, String> map = properties.entrySet().stream().collect(
+                Collectors.toMap(
+                        e -> String.valueOf(e.getKey()),
+                        e -> String.valueOf(e.getValue()),
+                        (prev, next) -> next, LinkedHashMap::new
+                ));
+
+        ApplicationJson applicationJson = new ApplicationJson(ApplicationProperties.builder()
+                .map(map)
+                .build());
+
+        Deployment deployment = new Deployment((ObjectNode) applicationJson.json());
+        log.debug("\n{}", deployment.json().toPrettyString());
     }
 }
