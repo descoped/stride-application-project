2d182b2260d32a9dba12135c46dc25e62dba2170
oranheim <oranheim@gmail.com>
2023-03-24 07:42:03 +0100

    Refactoring

--- stride-application/src/main/java/io/descoped/stride/application/StrideApplication.java
+++ stride-application/src/main/java/io/descoped/stride/application/StrideApplication.java
@@ -41,10 +41,12 @@ public class StrideApplication implements AutoCloseable {
     private final AtomicBoolean closed = new AtomicBoolean();
     private final ApplicationConfiguration configuration;
     private final ServiceLocator serviceLocator;
+    private final Lifecycle lifecycle;
 
     public StrideApplication(ApplicationProperties configuration) {
         this.configuration = new ApplicationConfiguration(configuration);
         this.serviceLocator = ServiceLocatorFactory.getInstance().create(null);
+        this.lifecycle = new Lifecycle(this.configuration, serviceLocator);
         //log.debug("Config:\n{}", this.configuration.toPrettyString());
     }
 
@@ -52,23 +54,15 @@ public class StrideApplication implements AutoCloseable {
         return serviceLocator;
     }
 
-    public int geHost() {
-        int port = -1;
-        Server server = serviceLocator.getService(Server.class);
-        for (Connector connector : server.getConnectors()) {
-            // the first connector should be the http connector
-            ServerConnector serverConnector = (ServerConnector) connector;
-            List<String> protocols = serverConnector.getProtocols();
-            if (!protocols.contains("ssl") && (protocols.contains("http/1.1") || protocols.contains("h2c"))) {
-                port = serverConnector.getLocalPort();
-                break;
-            }
-        }
-        return port;
-    }
-
     private Optional<ServerConnector> getConnector() {
         Server server = serviceLocator.getService(Server.class);
+        if (server == null) {
+            int runLevel = ofNullable(serviceLocator.getService(RunLevelController.class))
+                    .map(RunLevelController::getCurrentRunLevel)
+                    .orElse(-1);
+            log.error("Jetty Server is NOT started (run-level: {})", runLevel);
+            return Optional.empty();
+        }
         for (Connector connector : server.getConnectors()) {
             // the first connector should be the http connector
             ServerConnector serverConnector = (ServerConnector) connector;
@@ -112,16 +106,87 @@ public class StrideApplication implements AutoCloseable {
         stop();
     }
 
+    static class Lifecycle {
+        private final ApplicationConfiguration configuration;
+        private final ServiceLocator serviceLocator;
+        private final BeanDiscovery beanDiscovery;
+
+        Lifecycle(ApplicationConfiguration configuration, ServiceLocator serviceLocator) {
+            this.configuration = configuration;
+            this.serviceLocator = serviceLocator;
+            beanDiscovery = new BeanDiscovery(configuration, serviceLocator);
+        }
+
+        void configure() {
+            RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
+            runLevelController.setThreadingPolicy(RunLevelController.ThreadingPolicy.valueOf(configuration.asString("hk2.threadpolicy", "FULLY_THREADED")));
+            runLevelController.setMaximumUseableThreads(configuration.asInt("hk2.threadcount", 20));
+        }
+
+        void preStart() {
+            beanDiscovery.populateServiceLocator();
+
+            configure();
+
+            RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
+            if (runLevelController.getMaximumUseableThreads() > 3) {
+                runLevelController.proceedTo(3); // start run levels before jetty server start
+            }
+        }
+
+        void start() {
+            RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
+            runLevelController.proceedTo(runLevelController.getMaximumUseableThreads()); // start all run levels
+        }
+
+        void preStop() {
+            RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
+            runLevelController.proceedTo(0);
+        }
+
+        void stop() {
+            serviceLocator.shutdown();
+        }
+    }
+
+    static class BeanDiscovery {
+        private final ApplicationConfiguration configuration;
+        private final ServiceLocator serviceLocator;
+
+        BeanDiscovery(ApplicationConfiguration configuration, ServiceLocator serviceLocator) {
+            this.configuration = configuration;
+            this.serviceLocator = serviceLocator;
+            populateServiceLocator();
+        }
+
+        void populateServiceLocator() throws MultiException {
+            DynamicConfigurationService dcs = serviceLocator.getService(DynamicConfigurationService.class);
+
+            DynamicConfiguration dynamicConfiguration = ServiceLocatorUtilities.createDynamicConfiguration(serviceLocator);
+            dynamicConfiguration.addActiveDescriptor(BuilderHelper.createConstantDescriptor(configuration));
+            dynamicConfiguration.addActiveDescriptor(BuilderHelper.createConstantDescriptor(this));
+            dynamicConfiguration.addActiveDescriptor(DefaultTopicDistributionService.class);
+            dynamicConfiguration.commit();
+
+            Populator populator = dcs.getPopulator();
+
+            try {
+                populator.populate(
+                        new ClasspathDescriptorFileFinder()
+                        , new DuplicatePostProcessor()
+                        , new DefaultConfigurationPostPopulatorProcessor(configuration)
+                );
+            } catch (IOException e) {
+                throw new MultiException(e);
+            }
+        }
+    }
+
     private void doStart() {
-        populateServiceLocator(serviceLocator);
+        lifecycle.preStart();
+        lifecycle.start();
 
         Filter configurationFilter = getEnabledServicesFilter();
-
-        RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
-        runLevelController.setThreadingPolicy(RunLevelController.ThreadingPolicy.valueOf(configuration.asString("hk2.threadpolicy", "FULLY_THREADED")));
-        runLevelController.setMaximumUseableThreads(configuration.asInt("hk2.threadcount", 5));
-        runLevelController.proceedTo(configuration.asInt("hk2.runlevel", 20));
-
         List<ServiceHandle<?>> services = serviceLocator.getAllServiceHandles(configurationFilter);
         if (log.isDebugEnabled() && configuration.isVerboseLogging()) {
             StringBuilder msg = new StringBuilder();
@@ -139,34 +204,6 @@ public class StrideApplication implements AutoCloseable {
         }
     }
 
-    private void doStop() {
-        RunLevelController runLevelController = serviceLocator.getService(RunLevelController.class);
-        runLevelController.proceedTo(0);
-        serviceLocator.shutdown();
-    }
-
-    private void populateServiceLocator(ServiceLocator serviceLocator) throws MultiException {
-        DynamicConfigurationService dcs = serviceLocator.getService(DynamicConfigurationService.class);
-
-        DynamicConfiguration dynamicConfiguration = ServiceLocatorUtilities.createDynamicConfiguration(serviceLocator);
-        dynamicConfiguration.addActiveDescriptor(BuilderHelper.createConstantDescriptor(configuration));
-        dynamicConfiguration.addActiveDescriptor(BuilderHelper.createConstantDescriptor(this));
-        dynamicConfiguration.addActiveDescriptor(DefaultTopicDistributionService.class);
-        dynamicConfiguration.commit();
-
-        Populator populator = dcs.getPopulator();
-
-        try {
-            populator.populate(
-                    new ClasspathDescriptorFileFinder()
-                    , new DuplicatePostProcessor()
-                    , new DefaultConfigurationPostPopulatorProcessor(configuration)
-            );
-        } catch (IOException e) {
-            throw new MultiException(e);
-        }
-    }
-
     Filter getEnabledServicesFilter() {
         return descriptor -> {
             boolean defaultValue = configuration.asBoolean("hk2.defaults.enabled", false);
@@ -181,6 +218,11 @@ public class StrideApplication implements AutoCloseable {
             }
             return enabled;
         };
+    }
+
+    private void doStop() {
+        lifecycle.preStop();
+        lifecycle.stop();
     }
 
     // ---------------------------------------------------------------------------------------------------------------
