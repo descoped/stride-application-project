1c5ae03f7ac09284262d862de18f8f4597084516
oranheim <oranheim@gmail.com>
2023-04-23 16:19:51 +0200

    Refactoring

--- stride-application-api/src/main/java/io/descoped/stride/application/api/ApplicationConfigurator.java
+++ stride-application-api/src/main/java/io/descoped/stride/application/api/ApplicationConfigurator.java
@@ -3,17 +3,15 @@ package io.descoped.stride.application.api;
 import io.descoped.stride.application.config.api.ApplicationConfiguration;
 import io.descoped.stride.application.spi.ApplicationInitializer;
 
-import java.util.Optional;
 import java.util.ServiceLoader;
 
 class ApplicationConfigurator {
 
     StrideApplication configure(ApplicationConfiguration configuration) {
         ServiceLoader<ApplicationInitializer> serviceLoader = ServiceLoader.load(ApplicationInitializer.class);
-        Optional<ApplicationInitializer> initializer = serviceLoader.findFirst();
-        if (initializer.isEmpty()) {
-            throw new RuntimeException("Missing SPI for " + ApplicationInitializer.class.getName());
-        }
-        return initializer.get().initialize(configuration);
+        ApplicationInitializer initializer = serviceLoader
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Missing SPI for " + ApplicationInitializer.class.getName()));
+        return initializer.initialize(configuration);
     }
 }
